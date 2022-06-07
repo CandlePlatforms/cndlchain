47e549f541d99852cc914c11282b5ff9b551513a
kris-fantohm <cryptodev0104@gmail.com>
2022-06-07 08:25:20 -0400

    Add constant

---
+++ constants/currencies.ts
@@ -0,0 +1,71 @@
+export const currencies = [
+  {
+      name: 'eth',
+      full: 'Ether - PoS',
+  },
+  {
+      name: 'usdc',
+      full: 'USD Coin',
+  },
+  {
+      name: 'usdt',
+      full: 'Tether USD',
+  },
+  {
+      name: 'dai',
+      full: 'Dai - PoS',
+  },
+  {
+      name: 'aave',
+      full: 'Aave',
+  },
+  {
+      name: 'link',
+      full: 'ChainLink Token',
+  },
+  {
+      name: 'wbtc',
+      full: 'Wrapped BTC',
+  },
+  {
+      name: 'uni',
+      full: 'Uniswap',
+  },
+  {
+      name: 'sushi',
+      full: 'SushiToken',
+  },
+  {
+      name: 'quick',
+      full: 'Quickswap',
+  },
+  {
+      name: 'ghst',
+      full: 'Aavegotchi GHST Token',
+  },
+  {
+      name: 'tel',
+      full: 'Telcoin',
+  },
+  {
+      name: 'bal',
+      full: 'Balancer',
+  },
+  {
+      name: 'busd',
+      full: 'Binance USD',
+  },
+  {
+      name: 'dg',
+      full: 'Decentral Games',
+  },
+  {
+      name: 'mana',
+      full: 'Decentraland MANA',
+  },
+  {
+      name: 'matic',
+      full: 'Matic Token',
+      plasma: true,
+  },
+];
--- pages/wallets/dashboard/assets.tsx
+++ pages/wallets/dashboard/assets.tsx
@@ -1,82 +1,12 @@
 import { DashboardLayout } from '../../../components/layouts/DashboardLayout';
 import { useState } from 'react';
+import { currencies } from '../../../constants/currencies';
 
 AssetsPage.getLayout = (page: any) => {
     return <DashboardLayout>{page}</DashboardLayout>;
 };
 
 export default function AssetsPage() {
-    const currencies = [
-        {
-            name: 'eth',
-            full: 'Ether - PoS',
-        },
-        {
-            name: 'usdc',
-            full: 'USD Coin',
-        },
-        {
-            name: 'usdt',
-            full: 'Tether USD',
-        },
-        {
-            name: 'dai',
-            full: 'Dai - PoS',
-        },
-        {
-            name: 'aave',
-            full: 'Aave',
-        },
-        {
-            name: 'link',
-            full: 'ChainLink Token',
-        },
-        {
-            name: 'wbtc',
-            full: 'Wrapped BTC',
-        },
-        {
-            name: 'uni',
-            full: 'Uniswap',
-        },
-        {
-            name: 'sushi',
-            full: 'SushiToken',
-        },
-        {
-            name: 'quick',
-            full: 'Quickswap',
-        },
-        {
-            name: 'ghst',
-            full: 'Aavegotchi GHST Token',
-        },
-        {
-            name: 'tel',
-            full: 'Telcoin',
-        },
-        {
-            name: 'bal',
-            full: 'Balancer',
-        },
-        {
-            name: 'busd',
-            full: 'Binance USD',
-        },
-        {
-            name: 'dg',
-            full: 'Decentral Games',
-        },
-        {
-            name: 'mana',
-            full: 'Decentraland MANA',
-        },
-        {
-            name: 'matic',
-            full: 'Matic Token',
-            plasma: true,
-        },
-    ];
     const [page, setPage] = useState(1);
     const [perpage, setPerPage] = useState(10);
     const total = currencies.length / perpage;
